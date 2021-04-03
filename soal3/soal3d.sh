a9f6b8fe15dc8cef8a773350820139febac88828
ThomasFelix <37539546+ThomasFel@users.noreply.github.com>
2021-04-04 01:31:07 +0700

    Upload soal3d.sh (FIX PLIS)

--- soal3/soal3d.sh
+++ soal3/soal3d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pass=$(date +"%m%d%Y")
-file=$(ls | grep -E "Kelinci_|Kucing_")
+file=$(ls | grep -E "Kelinci_|Kucing_|-|Foto.log|Koleksi_")
 
 zip -r -P $pass Koleksi.zip $file
 rm -r $file
